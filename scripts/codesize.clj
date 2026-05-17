9f3d8d6d183522b9479787367a50aef53d2ba6b9
PEZ <pez@pezius.com>
2026-05-17 18:28:55 +0200

    Add codesize task

---
+++ scripts/codesize.clj
@@ -0,0 +1,64 @@
+(ns codesize
+  (:require [babashka.fs :as fs]
+            [babashka.process :as p]
+            [clojure.string :as str]))
+
+(def build-dir "build/classes/java/main")
+(def codesize-jar "CodeSizeUtility/CodeSizeUtility.jar")
+
+(defn- namespace->path
+  "Converts a namespace like pez.mini.Pugilist to pez/mini/Pugilist"
+  [ns-str]
+  (str/replace ns-str "." "/"))
+
+(defn- find-class-files
+  "Find all .class files belonging to a bot namespace.
+   A bot like pez.mini.Pugilist may have inner/package-private classes
+   (Wave, EnemyWave) in the same directory."
+  [ns-str]
+  (let [ns-path (namespace->path ns-str)
+        class-dir (str build-dir "/" (fs/parent ns-path))
+        main-class (str (fs/file-name ns-path) ".class")]
+    (when (fs/exists? class-dir)
+      (->> (fs/list-dir class-dir)
+           (map str)
+           (filter #(str/ends-with? % ".class"))))))
+
+(defn- find-properties-file [ns-str]
+  (let [props-path (str build-dir "/" (namespace->path ns-str) ".properties")]
+    (when (fs/exists? props-path) props-path)))
+
+(defn- create-bot-jar!
+  "Create a temporary jar containing only the bot's classes and properties."
+  [ns-str]
+  (let [abs-build (str (fs/absolutize build-dir))
+        jar-path (str (fs/absolutize (str ".tmp/" (str/replace ns-str "." "-") ".jar")))
+        class-files (find-class-files ns-str)
+        props-file (find-properties-file ns-str)]
+    (when (empty? class-files)
+      (println (str "No class files found for " ns-str " in " build-dir))
+      (println "Run `bb build` first.")
+      (System/exit 1))
+    (fs/create-dirs ".tmp")
+    (let [entries (cond-> (mapv (fn [f] (subs (str (fs/absolutize f)) (inc (count abs-build)))) class-files)
+                    props-file (conj (subs (str (fs/absolutize props-file)) (inc (count abs-build)))))]
+      (apply p/shell {:dir build-dir}
+             "jar" "cf" jar-path
+             entries))
+    jar-path))
+
+(defn check! [{:keys [bot]}]
+  (when-not bot
+    (println "Usage: bb codesize <namespace>")
+    (println "Example: bb codesize pez.mini.Pugilist")
+    (System/exit 1))
+  (when-not (fs/exists? build-dir)
+    (println "Build output not found. Run `bb build` first.")
+    (System/exit 1))
+  (let [jar-path (create-bot-jar! bot)
+        result (p/shell {:out :string :in (java.io.ByteArrayInputStream. (.getBytes "exit\n"))}
+                        "java" "-jar" codesize-jar jar-path)
+        lines (str/split-lines (:out result))
+        size-line (first (filter #(str/includes? % "Codesize") lines))]
+    (when size-line (println size-line))
+    (fs/delete jar-path)))
---
+++ scripts/robocode.clj
@@ -0,0 +1,41 @@
+(ns robocode
+  (:require [babashka.fs :as fs]
+            [babashka.process :as p]))
+
+(def robocode-home
+  (str (fs/expand-home "~/robocode")))
+
+(defn- java-major-version []
+  (let [version-output (:out (p/shell {:out :string} "java" "-version"))
+        ;; java -version outputs to stderr
+        version-err (:err (p/shell {:err :string} "java" "-version"))
+        version-str (or (re-find #"\"([^\"]+)\"" version-err) "")
+        version-part (first (clojure.string/split (second (re-find #"\"([^\"]+)\"" version-err)) #"\+"))
+        major (first (clojure.string/split version-part #"\."))]
+    (if (= major "1")
+      (second (clojure.string/split version-part #"\."))
+      major)))
+
+(defn- java-options []
+  (let [major (parse-long (java-major-version))]
+    (when (and (> major 11) (< major 24))
+      ["-Djava.security.manager=allow"])))
+
+(defn launch! [_opts]
+  (let [java-opts (java-options)
+        env (if java-opts
+              {"_JAVA_OPTIONS" (clojure.string/join " " java-opts)}
+              {})]
+    (p/shell {:dir robocode-home
+              :extra-env env}
+             "java"
+             "-cp" "libs/*"
+             "-Xmx512M"
+             "-Xdock:name=Robocode"
+             "-Xdock:icon=robocode.ico"
+             "-XX:+IgnoreUnrecognizedVMOptions"
+             "--add-opens=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"
+             "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED"
+             "--add-opens=java.desktop/javax.swing.text=ALL-UNNAMED"
+             "--add-opens=java.desktop/sun.awt=ALL-UNNAMED"
+             "robocode.Robocode")))
