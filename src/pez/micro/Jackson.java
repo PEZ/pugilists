a2d57e09f36e4a5f9f1cc81318b5b12c721b0e46
PEZ <pez@pezius.com>
2026-05-23 18:40:34 +0200

    Revert "Experiment: use raw movement turn angle"
    This reverts commit 3378444ad24c57662c3ed845daa6ba3f7da97648.

--- src/pez/micro/Jackson.java
+++ src/pez/micro/Jackson.java
@@ -80,7 +80,7 @@ public class Jackson extends AdvancedRobot {
 			moveAngle += direction * 0.01;
 		double angle;
 		setAhead(Math.cos(angle = moveAngle - getHeadingRadians()) * 200);
-		setTurnRightRadians(angle);
+		setTurnRightRadians(Math.tan(angle));
 		// </movement>
 
 		// <gun>
