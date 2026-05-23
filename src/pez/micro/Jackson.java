ba13ffe0bc803d2b1d69492c7f421d0a2b3c7da1
PEZ <pez@pezius.com>
2026-05-23 18:10:51 +0200

    Experiment: compare projected orbit danger

--- src/pez/micro/Jackson.java
+++ src/pez/micro/Jackson.java
@@ -65,9 +65,8 @@ public class Jackson extends AdvancedRobot {
 		// <movement>
 		double forwardDanger = 0, reverseDanger = 0;
 		try {
-			int bin = enemyWave.hitBin(new Point2D.Double(myX, myY));
-			forwardDanger = enemyWave.surfFactors[bin];
-			reverseDanger = enemyWave.surfFactors[2 * MIDDLE_FACTOR - bin];
+			forwardDanger = enemyWave.surfFactors[enemyWave.hitBin(project(enemyAbsoluteBearing - direction * Math.PI / 2, 160))];
+			reverseDanger = enemyWave.surfFactors[enemyWave.hitBin(project(enemyAbsoluteBearing + direction * Math.PI / 2, 160))];
 		} catch (Exception ex) {
 		}
 		if (reverseDanger < forwardDanger) {
