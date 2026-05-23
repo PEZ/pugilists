ac474e9fdf273248b746290e3f7cca4acb818831
PEZ <pez@pezius.com>
2026-05-23 18:37:03 +0200

    Shrink: precompute half pi movement angle

--- src/pez/micro/Jackson.java
+++ src/pez/micro/Jackson.java
@@ -75,7 +75,7 @@ public class Jackson extends AdvancedRobot {
 		}
 		Rectangle2D fieldRectangle = new Rectangle2D.Double(WALL_MARGIN, WALL_MARGIN,
 				BATTLE_FIELD_WIDTH - WALL_MARGIN * 2, BATTLE_FIELD_HEIGHT - WALL_MARGIN * 2);
-		double moveAngle = enemyAbsoluteBearing - direction * Math.PI / 2;
+		double moveAngle = enemyAbsoluteBearing - direction * 1.5707963267948966;
 		while (!fieldRectangle.contains(project(moveAngle, 160)))
 			moveAngle += direction * 0.01;
 		double angle;
