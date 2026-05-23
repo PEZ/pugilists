177363bd6c71619d7b941715d60ae3df65e82004
PEZ <pez@pezius.com>
2026-05-23 12:32:49 +0200

    Revert "Experiment: reachable-bin proxy surfer for Aristocles (1139b)"
    This reverts commit e9d452dba426c40e7a721998136396f8d3171b38.

--- src/pez/micro/Aristocles.java
+++ src/pez/micro/Aristocles.java
@@ -18,6 +18,8 @@ public class Aristocles extends AdvancedRobot {
 	static final double MAX_BULLET_POWER = 3.0;
 	static final double BULLET_POWER = 1.9;
 	static final double WALL_MARGIN = 18;
+	static final double REVERSE_TUNER = 0.421075;
+	static final double WALL_BOUNCE_TUNER = 0.699484;
 
 	static final int DISTANCE_INDEXES = 10;
 	static final int VELOCITY_INDEXES = 10;
@@ -30,13 +32,10 @@ public class Aristocles extends AdvancedRobot {
 	static int timeSinceVChange;
 	static double enemyBearingDirection;
 	static int[][][][][] aimFactors = new int[DISTANCE_INDEXES][VELOCITY_INDEXES][VELOCITY_INDEXES][VCHANGE_TIME_INDEXES][FACTORS];
-	static Rectangle2D fieldRectangle = new Rectangle2D.Double(WALL_MARGIN, WALL_MARGIN,
-			BATTLE_FIELD_WIDTH - WALL_MARGIN * 2, BATTLE_FIELD_HEIGHT - WALL_MARGIN * 2);
-	static Point2D robotLocation;
-	static int[] surfFactors = new int[FACTORS];
-	static java.util.ArrayList<Wave> surfWaves = new java.util.ArrayList<Wave>();
 	static double direction = 1;
-	static double enemyFirePower = MAX_BULLET_POWER;
+	static double enemyFirePower;
+	static int GF1Hits;
+	static int tries;
 
 	public void run() {
 		setAdjustRadarForGunTurn(true);
@@ -49,48 +48,23 @@ public class Aristocles extends AdvancedRobot {
 		Wave wave = new Wave();
 		double enemyAbsoluteBearing = getHeadingRadians() + e.getBearingRadians();
 		double enemyDistance;
-		robotLocation = wave.gunLocation = new Point2D.Double(getX(), getY());
-		enemyLocation = project(robotLocation, enemyAbsoluteBearing, enemyDistance = e.getDistance());
-
-		Wave surfWave = new Wave();
-		surfWave.surf = true;
-		surfWave.gunLocation = enemyLocation;
-		surfWave.startBearing = enemyAbsoluteBearing + Math.PI;
-		surfWave.bulletPower = enemyFirePower;
-		surfWave.bearingDirection = Math.copySign(0.7 / MIDDLE_FACTOR,
-				getVelocity() * Math.sin(getHeadingRadians() - surfWave.startBearing));
-		surfWave.factors = surfFactors;
-		surfWaves.add(surfWave);
-		addCustomEvent(surfWave);
+		enemyLocation = project(wave.gunLocation = new Point2D.Double(getX(), getY()), enemyAbsoluteBearing, enemyDistance = e.getDistance());
 
 		// <movement>
-		try {
-			Wave closest = closestSurfWave();
-			int currentBin = closest.visitingIndex(robotLocation);
-			double gap = closest.gunLocation.distance(robotLocation) - closest.distanceFromGun;
-			int reachable = Math.max(1, (int)Math.min(MIDDLE_FACTOR, MAX_VELOCITY * Math.max(0, gap) /
-					bulletVelocity(closest.bulletPower) / closest.gunLocation.distance(robotLocation) /
-					Math.abs(closest.bearingDirection)));
-			int bestBin = currentBin;
-			double bestDanger = Double.POSITIVE_INFINITY;
-			int i = Math.max(0, currentBin - reachable);
-			do {
-				double danger = surfDanger(i);
-				if (danger < bestDanger) {
-					bestDanger = danger;
-					bestBin = i;
-				}
-			} while (++i <= Math.min(FACTORS - 1, currentBin + reachable));
-			if (bestBin != currentBin) {
-				direction = Math.copySign(1, (bestBin - currentBin) * closest.bearingDirection);
-			}
+		Point2D robotDestination;
+		Rectangle2D fieldRectangle = new Rectangle2D.Double(WALL_MARGIN, WALL_MARGIN,
+				BATTLE_FIELD_WIDTH - WALL_MARGIN * 2, BATTLE_FIELD_HEIGHT - WALL_MARGIN * 2);
+		tries = 0;
+		while (!fieldRectangle.contains(robotDestination = project(wave.gunLocation,
+				enemyAbsoluteBearing - direction * (Math.PI / 2 - tries / 100.0), 160)) && tries++ < 125);
+		double bv = bulletVelocity(enemyFirePower);
+		if (GF1Hits > 4 && (Math.random() < (bv / REVERSE_TUNER) / enemyDistance ||
+				tries > (enemyDistance / bv / WALL_BOUNCE_TUNER))) {
+			direction = -direction;
 		}
-		catch (Exception ex) {
-		}
-		Point2D robotDestination = wallSmoothedDestination(direction);
 		// Jamougha's cool way
 		double angle;
-		setAhead(Math.cos(angle = absoluteBearing(robotLocation, robotDestination) - getHeadingRadians()) * 200);
+		setAhead(Math.cos(angle = absoluteBearing(wave.gunLocation, robotDestination) - getHeadingRadians()) * 200);
 		setTurnRightRadians(Math.tan(angle));
 		// </movement>
 
@@ -135,13 +109,10 @@ public class Aristocles extends AdvancedRobot {
 	}
 
 	public void onHitByBullet(HitByBulletEvent e) {
+		if (tries < 30) {
+			GF1Hits++;
+		}
 		enemyFirePower = e.getPower();
-		try {
-			Wave closest = closestSurfWave();
-			closest.factors[closest.visitingIndex(robotLocation)] += 3;
-		}
-		catch (Exception ex) {
-		}
 	}
 
 	static double bulletVelocity(double power) {
@@ -157,37 +128,6 @@ public class Aristocles extends AdvancedRobot {
 		return Math.atan2(target.getX() - source.getX(), target.getY() - source.getY());
 	}
 
-	static Point2D wallSmoothedDestination(double movementDirection) {
-		int tries = 0;
-		Point2D destination;
-		double enemyAbsoluteBearing = absoluteBearing(robotLocation, enemyLocation);
-		while (!fieldRectangle.contains(destination = project(robotLocation,
-				enemyAbsoluteBearing - movementDirection * (Math.PI / 2 - tries / 100.0), 160)) && tries++ < 125);
-		return destination;
-	}
-
-	static double surfDanger(int index) {
-		double danger = 0;
-		int i = FACTORS;
-		do {
-			danger += surfFactors[--i] / (Math.abs(index - i) + 1.0);
-		} while (i > 0);
-		return danger;
-	}
-
-	Wave closestSurfWave() {
-		Wave closest = null;
-		double closestGap = Double.POSITIVE_INFINITY;
-		for (Wave surfWave : surfWaves) {
-			double gap = Math.abs(surfWave.gunLocation.distance(robotLocation) - surfWave.distanceFromGun);
-			if (gap < closestGap) {
-				closestGap = gap;
-				closest = surfWave;
-			}
-		}
-		return closest;
-	}
-
 	class Wave extends Condition {
 		double bulletPower;
 		Point2D gunLocation;
@@ -195,24 +135,12 @@ public class Aristocles extends AdvancedRobot {
 		double bearingDirection;
 		int[] factors;
 		double distanceFromGun;
-		boolean surf;
-
-		int visitingIndex(Point2D location) {
-			return (int)Math.round(((Utils.normalRelativeAngle(absoluteBearing(gunLocation, location) - startBearing)) /
-					bearingDirection) + MIDDLE_FACTOR);
-		}
 
 		public boolean test() {
-			distanceFromGun += bulletVelocity(bulletPower);
-			if (surf) {
-				if (distanceFromGun > gunLocation.distance(robotLocation) + 36) {
-					surfWaves.remove(this);
-					removeCustomEvent(this);
-				}
-			}
-			else if (distanceFromGun > gunLocation.distance(enemyLocation) - 18) {
+			if ((distanceFromGun += bulletVelocity(bulletPower)) > gunLocation.distance(enemyLocation) - 18) {
 				try {
-					factors[visitingIndex(enemyLocation)]++;
+					factors[(int)Math.round(((Utils.normalRelativeAngle(absoluteBearing(gunLocation, enemyLocation) - startBearing)) /
+							bearingDirection) + MIDDLE_FACTOR)]++;
 				}
 				catch (Exception e) {
 				}
