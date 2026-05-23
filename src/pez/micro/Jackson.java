79bdb5deff32f1627f071fe370ac37d19a3069b4
PEZ <pez@pezius.com>
2026-05-23 17:29:23 +0200

    WIP: Jackson, 860

--- src/pez/micro/Jackson.java
+++ src/pez/micro/Jackson.java
@@ -29,14 +29,13 @@ public class Jackson extends AdvancedRobot {
 
 	static Point2D currentEnemyLocation;
 	static double enemyBearingDirection;
-	static int[][][] aimFactors = new int[DISTANCE_INDEXES][VELOCITY_INDEXES][FACTORS];
+	static double myX, myY;
+	static int[][] aimFactors = new int[VELOCITY_INDEXES][FACTORS];
 	static int[][] realMovementFactors = new int[VELOCITY_INDEXES][FACTORS];
 	static double direction = 1;
 	static double enemyEnergy;
-	static double enemyFirePower = BULLET_POWER;
 	static double lastVelocity;
 	static Wave surfWave;
-	static int tries;
 
 	public void run() {
 		setAdjustRadarForGunTurn(true);
@@ -46,79 +45,58 @@ public class Jackson extends AdvancedRobot {
 	}
 
 	public void onScannedRobot(ScannedRobotEvent e) {
-		Wave wave = new Wave();
+		myX = getX();
+		myY = getY();
 		double enemyAbsoluteBearing = getHeadingRadians() + e.getBearingRadians();
-		double enemyDistance;
-		currentEnemyLocation = project(wave.gunLocation = new Point2D.Double(getX(), getY()), enemyAbsoluteBearing,
-				enemyDistance = e.getDistance());
-		int distanceIndex = (int) (enemyDistance / (MAX_DISTANCE / DISTANCE_INDEXES));
-		int movementVelocityIndex = (int) (Math.abs(lastVelocity) / (MAX_VELOCITY / VELOCITY_INDEXES));
-		double movementStartBearing = absoluteBearing(currentEnemyLocation, wave.gunLocation);
+		currentEnemyLocation = project(enemyAbsoluteBearing, e.getDistance());
+		Point2D myLocation = new Point2D.Double(myX, myY);
+		int movementVelocityIndex = (int) Math.abs(lastVelocity);
+		double movementStartBearing = absoluteBearing(currentEnemyLocation, myLocation);
 		double movementBearingDirection = Math.copySign(0.7 / MIDDLE_FACTOR,
 				lastVelocity * Math.sin(getHeadingRadians() - movementStartBearing));
 		lastVelocity = getVelocity();
 		double enemyDeltaEnergy = enemyEnergy - e.getEnergy();
 		if (enemyDeltaEnergy > 0 && enemyDeltaEnergy <= MAX_BULLET_POWER) {
-			enemyFirePower = enemyDeltaEnergy;
-			Wave enemyWave = new Wave();
-			enemyWave.isSurfWave = true;
+			Wave enemyWave = new Wave(currentEnemyLocation, enemyDeltaEnergy,
+					movementStartBearing, movementBearingDirection);
 			enemyWave.surfFactors = realMovementFactors[movementVelocityIndex];
-			enemyWave.gunLocation = currentEnemyLocation;
-			enemyWave.bulletPower = enemyDeltaEnergy;
-			enemyWave.startBearing = movementStartBearing;
-			enemyWave.bearingDirection = movementBearingDirection;
 			enemyWave.distanceFromGun = 2 * bulletVelocity(enemyDeltaEnergy);
 			addCustomEvent(enemyWave);
 		}
 		enemyEnergy = e.getEnergy();
 
 		// <movement>
-		Point2D robotDestination;
+		double forwardDanger = 0, reverseDanger = 0;
+		try {
+			int bin = surfWave.hitBin(new Point2D.Double(myX, myY));
+			forwardDanger = surfWave.surfFactors[bin];
+			reverseDanger = surfWave.surfFactors[2 * MIDDLE_FACTOR - bin];
+		} catch (Exception ex) {
+		}
+		if (reverseDanger < forwardDanger) {
+			direction = -direction;
+		}
 		Rectangle2D fieldRectangle = new Rectangle2D.Double(WALL_MARGIN, WALL_MARGIN,
 				BATTLE_FIELD_WIDTH - WALL_MARGIN * 2, BATTLE_FIELD_HEIGHT - WALL_MARGIN * 2);
-		tries = 0;
-		while (!fieldRectangle.contains(robotDestination = project(wave.gunLocation,
-				enemyAbsoluteBearing - direction * (Math.PI / 2 - tries / 100.0), 160)) && tries++ < 125)
-			;
-		int forwardTries = tries;
-		Point2D reverseDestination;
-		tries = 0;
-		while (!fieldRectangle.contains(reverseDestination = project(wave.gunLocation,
-				enemyAbsoluteBearing + direction * (Math.PI / 2 - tries / 100.0), 160)) && tries++ < 125)
-			;
-		int reverseTries = tries;
-		Wave movementWave = surfWave;
-		double forwardDanger = movementDanger(movementWave, predictPosition(movementWave, direction, fieldRectangle));
-		double reverseDanger = movementDanger(movementWave, predictPosition(movementWave, -direction, fieldRectangle));
-
-		if (forwardTries > 65 || reverseDanger < forwardDanger) {
-			direction = -direction;
-			robotDestination = reverseDestination;
-			tries = reverseTries;
-		} else {
-			tries = forwardTries;
-		}
-		// Jamougha's cool way
+		double moveAngle = enemyAbsoluteBearing - direction * Math.PI / 2;
+		while (!fieldRectangle.contains(project(moveAngle, 160)))
+			moveAngle += direction * 0.01;
 		double angle;
-		setAhead(Math.cos(angle = absoluteBearing(wave.gunLocation, robotDestination) - getHeadingRadians()) * 200);
+		setAhead(Math.cos(angle = moveAngle - getHeadingRadians()) * 200);
 		setTurnRightRadians(Math.tan(angle));
 		// </movement>
 
 		// <gun>
 		double enemyVelocity = e.getVelocity();
-		int velocityIndex = (int) (Math.abs(enemyVelocity) / (MAX_VELOCITY / VELOCITY_INDEXES));
+		int velocityIndex = (int) Math.abs(enemyVelocity);
 
 		if (enemyVelocity != 0) {
 			enemyBearingDirection = Math.copySign(0.7 / MIDDLE_FACTOR,
 					enemyVelocity * Math.sin(e.getHeadingRadians() - enemyAbsoluteBearing));
 		}
-		wave.bearingDirection = enemyBearingDirection;
-
-		wave.bulletPower = Math.min(getEnergy() / 2, enemyFirePower);
-
-		wave.factors = aimFactors[distanceIndex][velocityIndex];
-
-		wave.startBearing = enemyAbsoluteBearing;
+		Wave wave = new Wave(myLocation, Math.min(getEnergy() / 2, 2),
+				enemyAbsoluteBearing, enemyBearingDirection);
+		wave.factors = aimFactors[velocityIndex];
 
 		int mostVisited = MIDDLE_FACTOR, i = FACTORS;
 		do {
@@ -140,61 +118,24 @@ public class Jackson extends AdvancedRobot {
 
 	public void onHitByBullet(HitByBulletEvent e) {
 		try {
-			surfWave.surfFactors[movementIndex(surfWave.gunLocation,
-					new Point2D.Double(getX(), getY()), surfWave.startBearing,
-					surfWave.bearingDirection)]++;
+			surfWave.surfFactors[surfWave.hitBin(new Point2D.Double(myX, myY))]++;
 		} catch (Exception ex) {
 		}
-	}
-
-	public void onBulletHit(BulletHitEvent e) {
-		enemyEnergy -= Rules.getBulletDamage(e.getBullet().getPower());
 	}
 
 	static double bulletVelocity(double power) {
 		return 20 - 3 * power;
 	}
 
-	static Point2D project(Point2D sourceLocation, double angle, double length) {
-		return new Point2D.Double(sourceLocation.getX() + Math.sin(angle) * length,
-				sourceLocation.getY() + Math.cos(angle) * length);
+	static Point2D project(double angle, double length) {
+		return new Point2D.Double(myX + Math.sin(angle) * length, myY + Math.cos(angle) * length);
 	}
 
 	static double absoluteBearing(Point2D source, Point2D target) {
 		return Math.atan2(target.getX() - source.getX(), target.getY() - source.getY());
 	}
 
-	int movementIndex(Point2D source, Point2D target, double startBearing, double bearingDirection) {
-		return (int) Math.round(((Utils.normalRelativeAngle(absoluteBearing(source, target) - startBearing)) /
-				bearingDirection) + MIDDLE_FACTOR);
-	}
-
-	double movementDanger(Wave wave, Point2D destination) {
-		try {
-			int index = movementIndex(wave.gunLocation, destination, wave.startBearing, wave.bearingDirection);
-			double danger = 0;
-			int i = FACTORS;
-			do {
-				danger += wave.surfFactors[--i] / (Math.abs(index - i) + 1.0);
-			} while (i > 0);
-			return danger;
-		} catch (Exception e) {
-			return 0;
-		}
-	}
-
-	Point2D predictPosition(Wave wave, double movementDirection, Rectangle2D fieldRectangle) {
-		if (wave == null) {
-			return new Point2D.Double(getX(), getY());
-		}
-		Point2D robotLocation = new Point2D.Double(getX(), getY());
-		double heading = absoluteBearing(robotLocation, currentEnemyLocation) - movementDirection * Math.PI / 2;
-		int ticks = Math.max(0, (int) ((wave.gunLocation.distance(robotLocation) - wave.distanceFromGun) /
-				bulletVelocity(wave.bulletPower)));
-		return project(robotLocation, heading, Math.min(ticks * (MAX_VELOCITY - 2), 160));
-	}
-
-	class Wave extends Condition {
+	static class Wave extends Condition {
 		double bulletPower;
 		Point2D gunLocation;
 		double startBearing;
@@ -202,26 +143,28 @@ public class Jackson extends AdvancedRobot {
 		int[] factors;
 		int[] surfFactors;
 		double distanceFromGun;
-		boolean isSurfWave;
+
+		Wave(Point2D gl, double bp, double sb, double bd) {
+			gunLocation = gl; bulletPower = bp; startBearing = sb; bearingDirection = bd;
+		}
+
+		int hitBin(Point2D target) {
+			return (int) Math.round(Utils.normalRelativeAngle(
+					absoluteBearing(gunLocation, target) - startBearing) / bearingDirection + MIDDLE_FACTOR);
+		}
 
 		public boolean test() {
-			if (isSurfWave) {
-				distanceFromGun += bulletVelocity(bulletPower);
-				double distance = gunLocation.distance(new Point2D.Double(getX(), getY()));
-				if (distanceFromGun < distance + 50 && surfWave == null) {
+			distanceFromGun += bulletVelocity(bulletPower);
+			if (surfFactors != null) {
+				if (distanceFromGun < gunLocation.distance(myX, myY) + 50 && surfWave == null) {
 					surfWave = this;
 				}
-				if (distanceFromGun > distance + 50) {
-					removeCustomEvent(this);
-				}
-			} else if ((distanceFromGun += bulletVelocity(bulletPower)) > gunLocation.distance(currentEnemyLocation) - 18) {
+			} else if (factors != null && distanceFromGun > gunLocation.distance(currentEnemyLocation) - 18) {
 				try {
-					factors[(int) Math
-							.round(((Utils.normalRelativeAngle(absoluteBearing(gunLocation, currentEnemyLocation) - startBearing)) /
-									bearingDirection) + MIDDLE_FACTOR)]++;
+					factors[hitBin(currentEnemyLocation)]++;
 				} catch (Exception e) {
 				}
-				removeCustomEvent(this);
+				factors = null;
 			}
 			return false;
 		}
