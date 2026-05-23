4951e644a125e04c86539b51e2573c77cc38fd54
PEZ <pez@pezius.com>
2026-05-23 08:21:53 +0200

    PK-4: P+K + 15x real wave weighting (732b)

--- src/pez/micro/Aristocles.java
+++ src/pez/micro/Aristocles.java
@@ -110,7 +110,7 @@ public class Aristocles extends AdvancedRobot {
 		setTurnGunRightRadians(Utils.normalRelativeAngle(enemyAbsoluteBearing - getGunHeadingRadians() +
 				wave.bearingDirection * (mostVisited - MIDDLE_FACTOR)));
 
-		if (setFireBullet(wave.bulletPower) != null) wave.weight = 5;
+		if (setFireBullet(wave.bulletPower) != null) wave.weight = 15;
 		addCustomEvent(wave);
 		// </gun>
 
