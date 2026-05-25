2a8f6fed3d6ab912f0369de3d4fe5eb18c8cf709
PEZ <pez@pezius.com>
2026-05-25 20:17:48 +0200

    Experiment: SC-1 SS-1 bullet policy

--- src/pez/mini/Pugilist.java
+++ src/pez/mini/Pugilist.java
@@ -91,8 +91,7 @@ public class Pugilist extends AdvancedRobot {
             enemyTimeSinceVChange = 0;
         }
 
-        double bulletPower = Math.min(enemyEnergy / 4,
-            distanceIndex > 0 ? BULLET_POWER : MAX_BULLET_POWER);
+        double bulletPower = enemyDistance < 175 ? MAX_BULLET_POWER : Math.clamp(enemyFirePower - 0.175, 0.1, 1.9);
 
         if (enemyVelocity != 0) {
             enemyBearingDirection = sign(enemyVelocity * Math.sin(e.getHeadingRadians() - enemyAbsoluteBearing));
@@ -109,7 +108,7 @@ public class Pugilist extends AdvancedRobot {
             wave.bearingDirection * (W.bestGF() - W.MIDDLE_FACTOR)));
 
         addCustomEvent(wave);
-        if (getEnergy() >= BULLET_POWER && Math.abs(getGunTurnRemainingRadians()) < Math.atan2(18, enemyDistance)) {
+        if (getEnergy() >= bulletPower && Math.abs(getGunTurnRemainingRadians()) < 18.0 / enemyDistance) {
             setFire(bulletPower);
         }
         // </gun>
