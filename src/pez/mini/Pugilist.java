98776ac7b99559d48870ef43954aef2629ed322a
PEZ <pez@pezius.com>
2026-05-26 15:05:26 +0200

    Pugilist: Remove GF0 priming

--- src/pez/mini/Pugilist.java
+++ src/pez/mini/Pugilist.java
@@ -178,7 +178,7 @@ public class Pugilist extends AdvancedRobot {
         static ArrayList<double[]> gunObss = new ArrayList<double[]>();
         static ArrayList<double[]> surfObss = new ArrayList<double[]>();
         static double[] scores = new double[FACTORS];
-        static { scores[DIM_GF] = MIDDLE_FACTOR; surfObss.add(scores); }
+        // static { scores[DIM_GF] = MIDDLE_FACTOR; surfObss.add(scores); } // Costs 18 bytes, buys zero APS
         static double dangerForward;
         static double dangerReverse;
         static Wave passingWave;
