f435819acc8ee95d0627e9f37df3ffa86f096cc8
PEZ <pez@pezius.com>
2026-05-31 18:16:07 +0200

    Exp F: precise tick-by-tick prediction in impactLocation()
    Replace simple MAX_VELOCITY projection with physics-accurate prediction:
    - Turn rate limited by Rules.getTurnRateRadians()
    - Acceleration ±1 (same dir) / ±2 (braking)
    - Heading tracked across prediction ticks
    
    Komarious-inspired. 1533 bytes (+72 over limit, not shippable as-is).

--- src/pez/mini2/Pugilist.java
+++ src/pez/mini2/Pugilist.java
@@ -285,11 +285,22 @@ public class Pugilist extends AdvancedRobot {
 
         Point2D impactLocation(int direction, int timeOffset) {
             Point2D loc = robotLocation;
+            double heading = robot.getHeadingRadians();
+            double velocity = robot.getVelocity();
+            double orbDir = direction * robot.robotBearingDirection(gunBearing(robotLocation));
             do {
-                loc = project(loc, absoluteBearing(loc,
-                        wallSmoothedDestination(loc,
-                                direction * robot.robotBearingDirection(gunBearing(robotLocation)))),
-                        MAX_VELOCITY);
+                double moveAngle = absoluteBearing(loc,
+                        wallSmoothedDestination(loc, orbDir)) - heading;
+                double moveDir = 1;
+                if (Math.cos(moveAngle) < 0) {
+                    moveAngle += Math.PI;
+                    moveDir = -1;
+                }
+                double maxTurn = Rules.getTurnRateRadians(Math.abs(velocity));
+                heading = Utils.normalRelativeAngle(heading +
+                        Math.clamp(Utils.normalRelativeAngle(moveAngle), -maxTurn, maxTurn));
+                velocity = Math.clamp(velocity + (velocity * moveDir < 0 ? 2 * moveDir : moveDir), -8, 8);
+                loc = project(loc, heading, velocity);
                 timeOffset++;
             } while (distanceFromTarget(loc, timeOffset) > -8);
             return loc;
