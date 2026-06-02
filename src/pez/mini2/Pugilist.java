b9cacc75ad07d0f26ab9d98791c03f56604b4f50
PEZ <pez@pezius.com>
2026-06-02 19:17:09 +0200

    Exp R4-6: rammer-gated max bullet power (cap150 base)

--- src/pez/mini2/Pugilist.java
+++ src/pez/mini2/Pugilist.java
@@ -112,7 +112,7 @@ public class Pugilist extends AdvancedRobot {
         }
 
         double bulletPower = Math.min(enemyEnergy / 4,
-                enemyDistance < 175 ? MAX_BULLET_POWER
+                ramLean > 0 || enemyDistance < 175 ? MAX_BULLET_POWER
                         : Math.clamp(Math.min(enemyFirePower - 0.175, 700 / enemyDistance), 0.1, BULLET_POWER));
 
         if (enemyVelocity != 0) {
